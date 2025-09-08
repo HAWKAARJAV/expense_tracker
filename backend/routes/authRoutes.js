7f38a84ed8c3cbf4778ccfb59dc2d18cc311edec
Aarjav Jain <iaarjavjain09@gmail.com>
2025-09-08 13:17:51 +0530

    Fix endpoint path for user information retrieval in authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -10,7 +10,7 @@ const { registerUser,
       const {protect}=require("../middleware/authMiddleware");
       router.post("/register", registerUser);
       router.post("/login", loginUser);
-      router.get("/getUserInfo", protect,getUserInfo);
+      router.get("/getUser", protect,getUserInfo);
         router.post("/upload-image", upload.single("image"), (req, res) => {
             if (!req.file) {
                 return res.status(400).json({ message: "No file uploaded" });
